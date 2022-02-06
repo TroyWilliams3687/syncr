b12ac115dfcf6714586df4867b0359c69ccec29e
Troy Williams <troy.williams@bluebill.net>
2022-02-06 15:00:03 -0500

    moved the os.walk into a generator that does the file validation. It returns a pathlib.Path representation of a file. Much easier to work with.

--- src/syncr/command_sync.py
+++ src/syncr/command_sync.py
@@ -33,15 +33,105 @@ console = Console()
 # Custom Modules
 
 from .common import print_folder_details
-
-# from .common import RichCommand
-
-# from .common import file_hash, write_json, read_json
-
-# from .create import create
-# from .clip import clip
-
 # -------------
+
+
+# https://gist.github.com/Esperanzq/3ee8a03e845c98bc12546272802cb41b
+# def get_all_files(path, min_size_kb=None):
+#     for root, dirs, files in os.walk(path):
+#         for file_ in files:
+#             path = os.path.abspath(os.path.join(root, file_))
+#             size = (os.path.getsize(path)) // 1024
+#             if size >= min_size_kb:
+#                 yield ("{} - {} kb".format(path, size))
+
+
+# files_lazy_lister = get_all_files("F:\\", min_size_kb=1000)
+# pprint.pprint(list(files_lazy_lister))
+
+def file_search(search_path, verbose=0, rules=None):
+    """
+    Uses os.walk and iterates through the path recursively following the
+    directory exclude patterns. It returns proper pathlib.Path objects
+    representing valid files.
+    """
+
+    for current_path, dirs, files in os.walk(search_path):
+
+        # current_path - the current path os.walk is on
+        # dirs - the folders in the current path
+        # files - the files in the current path
+
+        cp = Path(current_path)
+
+        if verbose >= 3:
+            console.print(f"[blue]Current Folder: {cp}[/blue]")
+
+        if "exclude-dir-pattern" in rules or "exclude-dir-path-pattern" in rules:
+
+            filtered_dir_list = []
+
+            for d in dirs:
+
+                add_directory = True
+
+                if "exclude-dir-pattern" in rules:
+                    if any(
+                        [
+                            fnmatch.fnmatch(d, pattern)
+                            for pattern in rules["exclude-dir-pattern"]
+                        ]
+                    ):
+
+                        # This test failed, skip the rest of the tests
+                        if verbose >= 2:
+                            console.print(f"[red]EXCLUDE-DIR -> {d}[/red]")
+
+                        add_directory = False
+
+                if "exclude-dir-path-pattern" in rules:
+
+                    if any(
+                        cp.joinpath(d) == pattern
+                        for pattern in rules["exclude-dir-path-pattern"]
+                    ):
+
+                        if verbose >= 2:
+                            console.print(f"[red]EXCLUDE-DIR -> {d}[/red]")
+
+                        add_directory = False
+
+                if add_directory:
+                    filtered_dir_list.append(d)
+
+            dirs[:] = filtered_dir_list
+
+        for f in files:
+
+            fp = cp.joinpath(f)
+
+            if "exclude-file-pattern" in rules:
+                if any(
+                    [
+                        fnmatch.fnmatch(f, pattern)
+                        for pattern in rules["exclude-file-pattern"]
+                    ]
+                ):
+                    continue
+
+            if "exclude-file-path-pattern" in rules:
+
+                if any(
+                    fp == pattern
+                    for pattern in rules["exclude-file-path-pattern"]
+                ):
+                    continue
+
+            yield fp
+
+
+
+
 
 
 @click.command()
@@ -93,117 +183,163 @@ def sync(*args, **kwargs):
                 for f in folder["exclude-file-path-pattern"]
             ]
 
-        for current_path, dirs, files in os.walk(source_path):
+        for source_file_path in file_search(source_path, verbose=kwargs['verbose'], rules=folder):
 
-            # current_path - the current path os.walk is on
-            # dirs - the folders in the current path
-            # files - the files in the current path
+            destination_file_path = destination_path.joinpath(
+                source_file_path.relative_to(source_path)
+            )
 
-            cp = Path(current_path)
 
-            if kwargs["verbose"] >= 3:
-                console.print(f"[blue]Current Folder: {cp}[/blue]")
+            if destination_file_path.exists():
 
-            # ------------
+                # WE could probably do an SHA 256 HASH on the file to really compare things.
 
-            if "exclude-dir-pattern" in folder or "exclude-dir-path-pattern" in folder:
+                # is the source file newer than the destination file?
+                if source_file_path.stat().st_mtime > destination_file_path.stat().st_mtime:
 
-                filtered_dir_list = []
+                    console.print(
+                        f"[green]EXISTS - OLDER[/green] -> {source_file_path.relative_to(source_path)}"
+                    )
 
-                for d in dirs:
+                else:
 
-                    add_directory = True
-
-                    if "exclude-dir-pattern" in folder:
-                        if any(
-                            [
-                                fnmatch.fnmatch(d, pattern)
-                                for pattern in folder["exclude-dir-pattern"]
-                            ]
-                        ):
-
-                            # This test failed, skip the rest of the tests
-                            if kwargs["verbose"] >= 2:
-                                console.print(f"[red]EXCLUDE-DIR -> {d}[/red]")
-
-                            add_directory = False
-
-                    if "exclude-dir-path-pattern" in folder:
-
-                        if any(
-                            cp.joinpath(d) == pattern
-                            for pattern in folder["exclude-dir-path-pattern"]
-                        ):
-                            # filtered_dir_list.append(d)
-
-                            if kwargs["verbose"] >= 2:
-                                console.print(f"[red]EXCLUDE-DIR -> {d}[/red]")
-
-                            add_directory = False
-
-                    if add_directory:
-                        filtered_dir_list.append(d)
-
-                dirs[:] = filtered_dir_list
-
-            for f in files:
-
-                source_file_path = cp.joinpath(f)
-
-                if "exclude-file-pattern" in folder:
-                    if any(
-                        [
-                            fnmatch.fnmatch(f, pattern)
-                            for pattern in folder["exclude-file-pattern"]
-                        ]
-                    ):
-                        continue
-
-                if "exclude-file-path-pattern" in folder:
-
-                    if any(
-                        source_file_path == pattern
-                        for pattern in folder["exclude-file-path-pattern"]
-                    ):
-                        continue
-
-                destination_file_path = destination_path.joinpath(
-                    cp.relative_to(source_path)
-                ).joinpath(f)
-
-                if destination_file_path.exists():
-
-                    # WE could probably do an SHA 256 HASH on the file to really compare things.
-
-                    # is the source file newer than the destination file?
-                    if source_file_path.stat().st_mtime > destination_file_path.stat().st_mtime:
-
+                    if kwargs["verbose"] >= 1:
                         console.print(
-                            f"[green]EXISTS - OLDER[/green] -> {source_file_path.relative_to(source_path)}"
+                            f"[red]EXISTS[/red] -> {source_file_path.relative_to(source_path)}"
                         )
 
-                    else:
+                    continue
 
-                        if kwargs["verbose"] >= 1:
-                            console.print(
-                                f"[red]EXISTS[/red] -> {source_file_path.relative_to(source_path)}"
-                            )
+            if source_file_path.is_file():
 
-                        continue
+                if not kwargs["dry_run"]:
 
-                if source_file_path.is_file():
+                    destination_file_path.parent.mkdir(parents=True, exist_ok=True)
 
-                    if not kwargs["dry_run"]:
+                    console.print(
+                        f"COPYING: {source_file_path.relative_to(source_path)}"
+                    )
+                    destination_file_path.write_bytes(source_file_path.read_bytes())
 
-                        destination_file_path.parent.mkdir(parents=True, exist_ok=True)
+                else:
 
-                        console.print(
-                            f"COPYING: {cp.relative_to(source_path).joinpath(f)}"
-                        )
-                        destination_file_path.write_bytes(source_file_path.read_bytes())
+                    console.print(
+                        f"COPYING ([cyan]DRY RUN[/cyan]): {source_file_path.relative_to(source_path)}"
+                    )
 
-                    else:
 
-                        console.print(
-                            f"COPYING ([cyan]DRY RUN[/cyan]): {cp.relative_to(source_path).joinpath(f)}"
-                        )
+        # -----------
+        # for current_path, dirs, files in os.walk(source_path):
+
+        #     # current_path - the current path os.walk is on
+        #     # dirs - the folders in the current path
+        #     # files - the files in the current path
+
+        #     cp = Path(current_path)
+
+        #     if kwargs["verbose"] >= 3:
+        #         console.print(f"[blue]Current Folder: {cp}[/blue]")
+
+        #     # ------------
+
+        #     if "exclude-dir-pattern" in folder or "exclude-dir-path-pattern" in folder:
+
+        #         filtered_dir_list = []
+
+        #         for d in dirs:
+
+        #             add_directory = True
+
+        #             if "exclude-dir-pattern" in folder:
+        #                 if any(
+        #                     [
+        #                         fnmatch.fnmatch(d, pattern)
+        #                         for pattern in folder["exclude-dir-pattern"]
+        #                     ]
+        #                 ):
+
+        #                     # This test failed, skip the rest of the tests
+        #                     if kwargs["verbose"] >= 2:
+        #                         console.print(f"[red]EXCLUDE-DIR -> {d}[/red]")
+
+        #                     add_directory = False
+
+        #             if "exclude-dir-path-pattern" in folder:
+
+        #                 if any(
+        #                     cp.joinpath(d) == pattern
+        #                     for pattern in folder["exclude-dir-path-pattern"]
+        #                 ):
+        #                     # filtered_dir_list.append(d)
+
+        #                     if kwargs["verbose"] >= 2:
+        #                         console.print(f"[red]EXCLUDE-DIR -> {d}[/red]")
+
+        #                     add_directory = False
+
+        #             if add_directory:
+        #                 filtered_dir_list.append(d)
+
+        #         dirs[:] = filtered_dir_list
+
+        #     for f in files:
+
+        #         source_file_path = cp.joinpath(f)
+
+        #         if "exclude-file-pattern" in folder:
+        #             if any(
+        #                 [
+        #                     fnmatch.fnmatch(f, pattern)
+        #                     for pattern in folder["exclude-file-pattern"]
+        #                 ]
+        #             ):
+        #                 continue
+
+        #         if "exclude-file-path-pattern" in folder:
+
+        #             if any(
+        #                 source_file_path == pattern
+        #                 for pattern in folder["exclude-file-path-pattern"]
+        #             ):
+        #                 continue
+
+        #         destination_file_path = destination_path.joinpath(
+        #             cp.relative_to(source_path)
+        #         ).joinpath(f)
+
+        #         if destination_file_path.exists():
+
+        #             # WE could probably do an SHA 256 HASH on the file to really compare things.
+
+        #             # is the source file newer than the destination file?
+        #             if source_file_path.stat().st_mtime > destination_file_path.stat().st_mtime:
+
+        #                 console.print(
+        #                     f"[green]EXISTS - OLDER[/green] -> {source_file_path.relative_to(source_path)}"
+        #                 )
+
+        #             else:
+
+        #                 if kwargs["verbose"] >= 1:
+        #                     console.print(
+        #                         f"[red]EXISTS[/red] -> {source_file_path.relative_to(source_path)}"
+        #                     )
+
+        #                 continue
+
+        #         if source_file_path.is_file():
+
+        #             if not kwargs["dry_run"]:
+
+        #                 destination_file_path.parent.mkdir(parents=True, exist_ok=True)
+
+        #                 console.print(
+        #                     f"COPYING: {cp.relative_to(source_path).joinpath(f)}"
+        #                 )
+        #                 destination_file_path.write_bytes(source_file_path.read_bytes())
+
+        #             else:
+
+        #                 console.print(
+        #                     f"COPYING ([cyan]DRY RUN[/cyan]): {cp.relative_to(source_path).joinpath(f)}"
+        #                 )
--- src/syncr/common.py
+++ src/syncr/common.py
@@ -19,19 +19,15 @@
 
 from pathlib import Path
 
-# from typing import TYPE_CHECKING, List, NoReturn, Optional, Tuple
-
-
 # ------------
 # 3rd Party - From PyPI
 
 import click
 
-from rich.console import Console  # , RenderableType
+from rich.console import Console
 
 console = Console()
 
-# from rich.text import Text
 # ------------
 # Custom Modules
 
@@ -107,106 +103,3 @@ def find_files(folder: Path = None):
             classified.setdefault("UNKOWN", []).append(p)
 
     return classified
-
-
-# # --------------
-# # https://github.com/cookiejar/cookietemple/blob/master/cookietemple/custom_cli/click.py
-
-# def blend_text(
-#     message: str, color1: Tuple[int, int, int], color2: Tuple[int, int, int]
-# ) -> Text:
-#     """Blend text from one color to another."""
-#     text = Text(message)
-#     r1, g1, b1 = color1
-#     r2, g2, b2 = color2
-#     dr = r2 - r1
-#     dg = g2 - g1
-#     db = b2 - b1
-#     size = len(text)
-#     for index in range(size):
-#         blend = index / size
-#         color = f"#{int(r1 + dr * blend):2X}{int(g1 + dg * blend):2X}{int(b1 + db * blend):2X}"
-#         text.stylize(color, index, index + 1)
-#     return text
-
-# # https://github.com/Textualize/rich-cli/blob/644937a392e32ce7a462ae79c9ab539b6ffb4b0b/src/rich_cli/__main__.py#L144-L218
-# # Eventually, this might be in a pip project, for now, just use this one
-
-# class RichCommand(click.Command):
-#     """Override Clicks help with a Richer version."""
-
-#     # TODO: Extract this in to a general tool, i.e. rich-click
-
-#     def format_help(self, ctx, formatter):
-
-#         from rich.highlighter import RegexHighlighter
-#         from rich.panel import Panel
-#         from rich.table import Table
-#         from rich.theme import Theme
-
-#         class OptionHighlighter(RegexHighlighter):
-#             highlights = [
-#                 r"(?P<switch>\-\w)",
-#                 r"(?P<option>\-\-[\w\-]+)",
-#             ]
-
-#         highlighter = OptionHighlighter()
-
-#         console = Console(
-#             theme=Theme(
-#                 {
-#                     "option": "bold cyan",
-#                     "switch": "bold green",
-#                 }
-#             ),
-#             highlighter=highlighter,
-#         )
-
-#         # console.print(
-#         #     f"[b]Rich CLI[/b] [magenta]v[/] 🤑\n\n[dim]Rich text and formatting in the terminal\n",
-#         #     justify="center",
-#         # )
-
-#         # console.print(
-#         #     "Usage: [b]rich[/b] [b][OPTIONS][/] [b cyan]<PATH,TEXT,URL, or '-'>\n"
-#         # )
-
-#         options_table = Table(highlight=True, box=None, show_header=False)
-
-#         for param in self.get_params(ctx)[1:]:
-
-#             if len(param.opts) == 2:
-#                 opt1 = highlighter(param.opts[1])
-#                 opt2 = highlighter(param.opts[0])
-
-#             else:
-#                 opt2 = highlighter(param.opts[0])
-#                 opt1 = Text("")
-
-#             if param.metavar:
-#                 opt2 += Text(f" {param.metavar}", style="bold yellow")
-
-#             options = Text(" ".join(reversed(param.opts)))
-#             help_record = param.get_help_record(ctx)
-
-#             if help_record is None:
-#                 help = ""
-
-#             else:
-#                 help = Text.from_markup(param.get_help_record(ctx)[-1], emoji=False)
-
-#             if param.metavar:
-#                 options += f" {param.metavar}"
-
-#             options_table.add_row(opt1, opt2, highlighter(help))
-
-#         console.print(
-#             Panel(
-#                 options_table, border_style="dim", title="Options", title_align="left"
-#             )
-#         )
-
-#         console.print(
-#             blend_text("♥ https://www.textualize.io", (32, 32, 255), (255, 32, 255)),
-#             justify="right",
-#         )
